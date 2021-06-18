90b4114093a8ca2648f2776563fb9055103e42ef
Eli Kamin <eli.kamin@hpe.com>
2021-06-18 13:03:41 -0500

    SAT-997: Consolidate MDlint files
    This adds more comments and adds more rules into the mdlint file
    used by the `make lint` command. Most notably start checking line
    length, except for in tables and code blocks. Also, modify the
    "ordered list prefix" rule to check for literal numbering, i.e.
    (1, 2, 3) rather than (1, 1, 1 ...). Finally, use an 'indent'
    value of 4 for checking MD007.

--- portal/developer-portal/lint/docs_lint.rb
+++ portal/developer-portal/lint/docs_lint.rb
@@ -4,7 +4,7 @@
 # Rules commented out are those that should be adhered to, but require lots of
 # work to clean up in existing .md.  Noting for future cleanup effort.
 #
-# These rules contribute to readabity of both md and rendered
+# These rules contribute to readability of both md and rendered
 # content, and improve consistency across documents.
 # Rule MD013 may guard against build breaks.
 
@@ -12,50 +12,53 @@
 rule 'MD001'
 # Header style
 rule 'MD003'
+# Unordered list style
+rule 'MD004'
 # Consistent indentation levels
 rule 'MD005'
 # Consistent ordered list indentation
-rule 'MD007'
-
-#Trailing spaces - valid, but will cause much work to clean up 
-#rule 'MD009'
-
-#Hard Tabs - valid, but will cause much work to clean up 
-#rule 'MD010'
-
+rule 'MD007', :indent => 4
+# Trailing spaces
+rule 'MD009'
+# Hard Tabs
+rule 'MD010'
 # Link syntax
 rule 'MD011'
-# Line length
-rule 'MD013', :line_length => 700
+# Line length (120 characters, except in tables and code blocks)
+rule 'MD013', :line_length => 120, :tables => false, :code_blocks => false
 # No space after hash on header
 rule 'MD018'
-# Mult space after hash on header
+# Multiple spaces after hash on atx style header
 rule 'MD019'
 # No space inside hashes
 rule 'MD020'
 # Multiple spaces inside hashes on closed atx style header
 rule 'MD021'
-
-#Headers should be surrounded by blank lines. Adds to readablity of md.
-#rule 'MD022'
-
+# Headers should be surrounded by blank lines. Adds to readability of md
+rule 'MD022'
 # Headers must start at beginning of line
 rule 'MD023'
-
+# Multiple headers with the same content, except in different parent sections
+rule 'MD024', :allow_different_nesting => true
+# Ordered list item prefix, expect ordered lists
+rule 'MD029', :style => :ordered
 # Spaces after list markers
-#rule 'MD030'
-
-#Fenced code should be surrounded by blank lines.  Adds to readability of md.
-#rule 'MD031'
-#Lists should be surrounded by blank lines. Adds to readability of md.
-#rule 'MD032'
-
-#Not practical because of urls in curl commands inside tables
-#rule 'MD034'
-
+rule 'MD030'
+# Fenced code should be surrounded by blank lines.  Adds to readability of md.
+rule 'MD031'
+# Lists should be surrounded by blank lines. Adds to readability of md.
+rule 'MD032'
+# No bare URLs
+rule 'MD034'
+# Horizontal rule style
 rule 'MD035'
-#emphasis in headers
-#rule 'MD036'
+# Use headers instead of **emphasis**
+rule 'MD036'
+# Spaces inside emphasis markers
+rule 'MD037'
+# Spaces inside code span elements
 rule 'MD038'
+# Spaces inside link text
 rule 'MD039'
+# Code blocks should have language specified
 rule 'MD040'
