507a08a85aa528888f8cc1e0d2683bf83b242be8
Kenton Shade, DevOps Consultant <kenton.shade@hpe.com>
2021-09-22 16:30:44 -0400

    Jira-STP-2844
    * Jenkinsfile updates nightly build
    * setup-versioning.sh updates page break for copyright
    * sat-versioning.sh updates for corret versioning process
    * .gitignore updates for .vscode
    * Updates for nightly builds
    
    [Ticket: STP-2844]

--- sat-versioning.sh
+++ sat-versioning.sh
@@ -6,11 +6,32 @@ set -x
 # # Generate a file for jenkins to use.
 # cat ${REPO_PATH}/sat-product-stream/.version > ${REPO_PATH}/sat-version.txt
 
-# Per Eli, depending on public repo when done in Jenkins:
-SAT_PRODUCT_STREAM_REPO="https://stash.us.cray.com/scm/sat/sat-product-stream.git"
-SAT_PRODUCT_STREAM_BRANCH="release/sat-2.1"
 REPO_PATH=$(git rev-parse --show-toplevel)
-SAT_VERSION=$(rm -rf sat-product-stream && git clone -q --depth 1 --branch $SAT_PRODUCT_STREAM_BRANCH $SAT_PRODUCT_STREAM_REPO sat-product-stream && cat sat-product-stream/.version)
-rm -rf sat-product-stream
+SAT_PRODUCT_STREAM_REPO="https://stash.us.cray.com/scm/sat/sat-product-stream.git"
+CURRENT_BRANCH=$(git branch --show-current)
+if [[ "$CURRENT_BRANCH" == *"master"* ]]; then
+	SAT_PRODUCT_STREAM_BRANCH="master"
+	SAT_VERSION=$(cd ${REPO_PATH} && rm -rf sat-product-stream && git clone -q --depth 1 --branch ${SAT_PRODUCT_STREAM_BRANCH} ${SAT_PRODUCT_STREAM_REPO} sat-product-stream && cat sat-product-stream/.version)
+elif [[ "$CURRENT_BRANCH" == *"release/"* ]]; then
+	# We need the latest release branch
+	cd ${REPO_PATH}
+	rm -rf sat-product-stream
+	git clone -q ${SAT_PRODUCT_STREAM_REPO} sat-product-stream
+	cd ${REPO_PATH}/sat-product-stream/
+	git branch --all | grep "remotes/origin/release/sat" | sort -nr > ../remotes.txt
+	SAT_PRODUCT_STREAM_BRANCH=$(head -n 1 ../remotes.txt | cut -d/ -f3-4)
+	SAT_VERSION=$(git checkout -q ${SAT_PRODUCT_STREAM_BRANCH} && cat .version)
+	rm ../remotes.txt
+else
+	SAT_VERSION="1.0.0"
+fi
+
+# Per Eli, depending on public repo when done in Jenkins:
+# SAT_PRODUCT_STREAM_REPO="https://stash.us.cray.com/scm/sat/sat-product-stream.git"
+# SAT_PRODUCT_STREAM_BRANCH="release/sat-2.1"
+# REPO_PATH=$(git rev-parse --show-toplevel)
+# SAT_VERSION=$(rm -rf sat-product-stream && git clone -q --depth 1 --branch $SAT_PRODUCT_STREAM_BRANCH $SAT_PRODUCT_STREAM_REPO sat-product-stream && cat sat-product-stream/.version)
+# rm -rf sat-product-stream
+
 # Generate a file for jenkins to use.
-echo $SAT_VERSION > ${REPO_PATH}/sat-version.txt
+echo ${SAT_VERSION} > ${REPO_PATH}/sat-version.txt
