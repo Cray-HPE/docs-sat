138d05e5902a68dc097c3b334e3a42495b8e1059
Kenton Shade, Git Consultant <kenton.shade@hpe.com>
2021-09-24 14:22:22 -0400

    STP-2844: Versioning and nightly bulds
    Removing Commented code

--- sat-versioning.sh
+++ sat-versioning.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -x
-# REPO_PATH=$(git rev-parse --show-toplevel)
-# git submodule update --init ${REPO_PATH}/sat-product-stream > /dev/null
-# SAT_VERSION=$(cd ${REPO_PATH}/sat-product-stream/ && git checkout -q release/sat-2.1 && cat .version)
-# # Generate a file for jenkins to use.
-# cat ${REPO_PATH}/sat-product-stream/.version > ${REPO_PATH}/sat-version.txt
 
 REPO_PATH=$(git rev-parse --show-toplevel)
 SAT_PRODUCT_STREAM_REPO="https://stash.us.cray.com/scm/sat/sat-product-stream.git"
@@ -26,12 +21,5 @@ else
 	SAT_VERSION="1.0.0"
 fi
 
-# Per Eli, depending on public repo when done in Jenkins:
-# SAT_PRODUCT_STREAM_REPO="https://stash.us.cray.com/scm/sat/sat-product-stream.git"
-# SAT_PRODUCT_STREAM_BRANCH="release/sat-2.1"
-# REPO_PATH=$(git rev-parse --show-toplevel)
-# SAT_VERSION=$(rm -rf sat-product-stream && git clone -q --depth 1 --branch $SAT_PRODUCT_STREAM_BRANCH $SAT_PRODUCT_STREAM_REPO sat-product-stream && cat sat-product-stream/.version)
-# rm -rf sat-product-stream
-
-# Generate a file for jenkins to use.
+# Generate a file for Jenkins to use.
 echo ${SAT_VERSION} > ${REPO_PATH}/sat-version.txt
